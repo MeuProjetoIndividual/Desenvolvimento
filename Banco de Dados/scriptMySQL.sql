249c929b18f37a8435e96037b5429fd3e870f47f
FabioHKC <fabio.cesare@sptech.school>
2024-12-02 09:41:56 -0300

    remoção de atributo não usado - cargo

--- Banco de Dados/scriptMySQL.sql
+++ Banco de Dados/scriptMySQL.sql
@@ -6,8 +6,7 @@ create table Usuario(
 	idUsuario int primary key auto_increment,
     nome varchar(45),
     email varchar(100) unique,
-    senha varchar(45),
-    cargo varchar(45)
+    senha varchar(45)
 );
 
 select * from Usuario;
